d6d8d0d3abd9d5e34f1ba61e16777673f9a7b6d1
Vedomom_ <momoprofessionally03@gmail.com>
2026-06-26 03:20:45 +0300

    Learned lighting and orbitcontrols

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { ThreeMFLoader } from 'three/examples/jsm/Addons.js';
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { array } from 'three/tsl';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(80, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -15,10 +17,37 @@ camera.position.setZ(30);
 renderer.render(scene, camera)
 
 const cshape = new THREE.IcosahedronGeometry(10, 1)
-const cmaterial = new THREE.MeshBasicMaterial({color: 0xE895DC, wireframe: true});
+const cmaterial = new THREE.MeshStandardMaterial({color: 0xE895DC});
 const icos = new THREE.Mesh(cshape, cmaterial);
+const pointlight = new THREE.PointLight(0xffffff,100)
+pointlight.position.set(10, 10, 10)
+
+const ambientlight = new THREE.AmbientLight(0xffffff)
+
+const plighthelper = new THREE.PointLightHelper(pointlight)
+const gridhelper = new THREE.GridHelper(200, 50)
+scene.add(pointlight, ambientlight, plighthelper, gridhelper)
 
 scene.add(icos)
+const controls = new OrbitControls(camera, renderer.domElement)
+
+
+function addstar() {
+    const sphere = new THREE.SphereGeometry(0.2, 24, 24);
+    const star_mat = new THREE.MeshStandardMaterial({color: 0xFFF8CC});
+    const star = new THREE.Mesh(sphere, star_mat);
+
+    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    
+    scene.add(star);
+}
+
+Array(200).fill().forEach(addstar)
+
+
+
 function animate() {
     requestAnimationFrame(animate);
 
@@ -26,6 +55,7 @@ function animate() {
     icos.rotation.y += 0.001;
     icos.rotation.z += 0.002;
 
+    controls.update()
     renderer.render(scene, camera);
 }
 
